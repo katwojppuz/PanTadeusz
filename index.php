2cb379715edc065eb9ec9d68507ac1e8ea227550
katwojppuz <katarzyna.wojcik@ppuz.edu.pl>
2022-12-06 10:20:39 +0100

    index.html changed into index.php

--- index.php
+++ index.php
@@ -17,22 +17,24 @@
             <div class="col-4">
                 <h2>Spis treści</h2>
                 <ul>
-                    <li><a href="./k1.html">Księga 1</a></li>
-                    <li><a href="./k2.html">Księga 2</a></li>
-                    <li><a href="./k3.html">Księga 3</a></li>
-                    <li><a href="./k4.html">Księga 4</a></li>
-                    <li><a href="./k5.html">Księga 5</a></li>
-                    <li><a href="./k6.html">Księga 6</a></li>
-                    <li><a href="./k7.html">Księga 7</a></li>
-                    <li><a href="./k8.html">Księga 8</a></li>
-                    <li><a href="./k9.html">Księga 9</a></li>
-                    <li><a href="./k10.html">Księga 10</a></li>
-                    <li><a href="./k11.html">Księga 11</a></li>
-                    <li><a href="./k12.html">Księga 12</a></li>
+                    <li><a href="./">Strona główna</li>
+                    <?php
+                        for ($k=1; $k<=12; $k++) { 
+                            print("<li><a href='./index.php?k=$k'>Księga $k</a></li>");
+                        }
+                    ?>
                 </ul>
             </div>
             <div class="col-8">
-                <img src="./pan_tadeusz.jpg" alt="Pan Tadeusz" class="img-fluid">
+                <?php
+                    if (isset($_GET['k'])) {
+                        $k = $_GET['k'];
+                        include_once("k$k.html");
+                    } else {
+                        print('<img src="./pan_tadeusz.jpg" alt="Pan Tadeusz" class="img-fluid">');
+                    }
+                ?>
+                
             </div>
         </div>
     </div>
